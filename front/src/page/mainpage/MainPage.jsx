0e7d16d84e133dde92840e5390380a1c73b32b96
Umin73 <97716008+Umin73@users.noreply.github.com>
2025-02-27 10:09:24 +0900

    Chore: 인벤토리 경로

--- front/src/page/mainpage/MainPage.jsx
+++ front/src/page/mainpage/MainPage.jsx
@@ -7,6 +7,7 @@ import BasicButton from "../../component/common/button/BasicButton";
 import MainMap from "../../component/mainpage/kakaomap/MainMap";
 import Container from "../../component/common/Container";
 import HalfButtonWrapper from "../../component/common/wrapper/HalfBasicButtonWrapper";
+import Inventory from "../inventory/Inventory";
 import { PAGE_URLS } from "../../constant/constant";
 export default function MainPage() {
   const { appState, dispatch } = useAppContext();
@@ -15,7 +16,7 @@ export default function MainPage() {
     {
       icon: "📦",
       description: "캐릭터와 펫을 보관중이에요!",
-      href: PAGE_URLS.not,
+      href: PAGE_URLS.Inventory,
     },
     {
       icon: "🥚",
