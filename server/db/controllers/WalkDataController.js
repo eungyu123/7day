e7fa45020da6e0fd43ccda42ffc87d9b079922ba
gudwns1812 <gudwns1812@naver.com>
2025-03-07 13:16:03 +0900

    Update WalkDataController.js

--- server/db/controllers/WalkDataController.js
+++ server/db/controllers/WalkDataController.js
@@ -1,6 +1,7 @@
 const WalkData = require("../models/WalkData");
 
 module.exports = {
+  //WalkData 생성
   createWalkData: async (req, res) => {
     const walkData = new WalkData({
       userId: req.params.userId,
@@ -12,7 +13,13 @@ module.exports = {
   },
   getWalkData: async (req, res) => {
     const startDate = req.body.startDate;
-    const endDate = req.body.endDate;
+    let endDate = req.body.endDate;
+
+    // endDate를 23:59:59로 설정
+    endDate = new Date(endDate);
+    endDate.setHours(23, 59, 59, 999); // 23:59:59.999로 설정
+
+    //date가 startDate와 endDate 사이에 있는 walkdata를 반환
     const walkData = await WalkData.find(
       {
         userId: req.params.userId,
@@ -20,30 +27,58 @@ module.exports = {
       },
       { _id: 0, __v: 0 }
     );
+    if (!walkData) {
+      return res.status(400).json({
+        type: "error",
+        message: "walkData searching failed",
+      });
+    }
     return walkData;
   },
   updateWalkData: async (req, res) => {
     try {
+      //req에서 오늘 날짜의 startDay,endDay를 body포함하여 오늘 날짜의 walkData 업데이트
       const walkData = await WalkData.findOneAndUpdate(
         {
           userId: req.params.userId,
-          date: req.body.date,
+          date: {
+            $gte: req.body.startDay,
+            $lte: req.body.endDay,
+          },
         },
-        { steps: req.body.steps },
+        { $inc: { steps: req.body.steps } },
         {
           new: true,
         }
       );
+      if (!walkData) {
+        console.log("new data");
+        const now = new Date().toISOString();
+        // walkData가 없으면 새로 생성
+        const newWalkData = new WalkData({
+          userId: req.params.userId,
+          date: now,
+          steps: req.body.steps,
+        });
+        await newWalkData.save();
+        return newWalkData;
+      }
       return walkData;
     } catch (error) {
       return res.status(400).send({ message: "WalkData not found" });
     }
   },
   deleteWalkData: async (req, res) => {
-    await WalkData.findOneAndDelete({
+    const result = await WalkData.findOneAndDelete({
       userId: req.params.userId,
       date: req.body.date,
     });
+    if (!result) {
+      res.status(400).json({
+        type: "error",
+        message: "Mission searching failed",
+      });
+    }
     return { message: "WalkData deleted" };
   },
 };
