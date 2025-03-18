78b92ee40b3c1882ffb22f94c093c26f74846ceb
gudwns1812 <gudwns1812@naver.com>
2025-03-18 15:43:25 +0900

    Delete dd.js

--- server/dd.js
+++
@@ -1,4 +0,0 @@
-const utcDate = new Date("2025-03-05T00:00:00Z"); // UTC 시간대에서 생성
-const korDate = new Date(utcDate.getTime() + 9 * 60 * 60 * 1000); // 한국 시간으로 변환
-
-console.log(korDate); // 한국 시간으로 출력
